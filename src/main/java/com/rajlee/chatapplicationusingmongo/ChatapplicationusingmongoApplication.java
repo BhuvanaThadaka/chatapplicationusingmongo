684bc6926743acfb5931b84c3be62159b903f7b4
Bhuvana <bhuvanathadaka19@gmail.com>
2024-07-31 16:49:19 +0530

    Initial commit

---
+++ src/main/java/com/rajlee/chatapplicationusingmongo/ChatapplicationusingmongoApplication.java
@@ -0,0 +1,13 @@
+package com.rajlee.chatapplicationusingmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatapplicationusingmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatapplicationusingmongoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/rajlee/chatapplicationusingmongo/user/Status.java
@@ -0,0 +1,5 @@
+package com.rajlee.chatapplicationusingmongo.user;
+
+public enum Status {
+    ONLINE,OFFLINE
+}
